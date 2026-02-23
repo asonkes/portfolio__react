35af2ff738cd47e3152245ecae3edf5b638f1761
Audrey Sonkes <a.sonkes@interface3.be>
2026-02-23 16:15:16 +0100

    photo ordinateur en ordre dans partie contact

--- frontend/src/components/layout/SplitScreen.jsx
+++ frontend/src/components/layout/SplitScreen.jsx
@@ -4,7 +4,7 @@ export const SplitScreen = (props) => {
   return (
     //Si on mets : ${bgImage && "bg-no-repeat bg-cover bg-center" ==> bgImage peut-être falsy et alors 'undefined' mis par défaut}
     <div
-      className={`w-6/12 
+      className={`w-full lg:w-6/12 
         ${bgImage ? "bg-no-repeat bg-cover bg-center" : ""} 
         ${className}`}
       style={bgImage && { backgroundImage: `url(${bgImage})` }}
--- frontend/src/components/sections/Contact.jsx
+++ frontend/src/components/sections/Contact.jsx
@@ -17,17 +17,38 @@ export const Contact = () => {
         <Title title="Contact" />
 
         <div className="w-full max-w-7xl flex flex-1 flex-col mx-auto items-center justify-center lg:flex-row border-4 border-red-400">
-          <SplitScreen className="flex justify-center items-center border-4 border-yellow-400">
+          <SplitScreen 
+            className="flex justify-center items-center border-4 border-yellow-400"
+          >
             {/** Alors ici, malgré qu'il y ai pas de paramètre, il FAUT une fonction flêchée, pk?
                 Car React attend une exécution de fonction dans le TEMPS
-                Qui se déroulera APRES, qd l'utilisateur aura inétragit...
+                Qui se déroulera APRES, qd l'utilisateur aura intéragit...
             */}
-            <img
-              src={isHover ? imgOrdiColor : imgOrdi}
-              alt="Image représentant un ordinateur"
+
+            <div 
+              className="relative"
               onMouseEnter={() => setIsHover(true)}
               onMouseLeave={() => setIsHover(false)}
-            />
+            >
+              {/** On met l'action 'transition-opacity duration-1000 ease-in-out' par défaut sur les 2
+               * car elle doit avoir lieu avant que l'opacity soit gérée
+               */}
+              <img
+                src={imgOrdi}
+                className={`transition-opacity duration-1000 ease-in-out ${
+                  isHover ? "opacity-0" : "opacity-100"
+                }`}
+              />
+              {/** On met l'action 'transition-opacity duration-1000 ease-in-out' par défaut sur les 2
+               * car elle doit avoir lieu avant que l'opacity soit gérée
+               */}
+              <img
+                src={imgOrdiColor}
+                className={`absolute inset-0 transition-opacity duration-1000 ease-in-out ${
+                  isHover ? "opacity-100" : "opacity-0"
+                }`}
+              />
+            </div>
           </SplitScreen>
           <SplitScreen>
             <Text className="border-4 text-fuchsia-300 text-center text-3xl tracking-wider p-5">
