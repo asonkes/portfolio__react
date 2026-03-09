91a36af247a7aa86c7bc449f7ecddf2c76e789c0
asonkes <audrey.sonkes@gmail.com>
2026-03-09 12:22:42 +0100

    dernière animation ok

--- frontend/src/components/sections/Home.jsx
+++ frontend/src/components/sections/Home.jsx
@@ -1,23 +1,28 @@
 /*******************************/
 /*** Composant session Home  ***/
 /*******************************/
-
+import Style from "../ui/home/TriangleTransparent/TriangleTransparent.module.css";
 import { SplitScreen } from "../layout/SplitScreen";
 import bgHome from "../../assets/images/background/splash-gouttes-eau_23-21478283951.webp";
 import { Triangle } from "../ui/home/Triangle/Triangle";
 import { TriangleTwo } from "../ui/home/TriangleTwo/TriangleTwo";
 import { TriangleTransparent } from "../ui/home/TriangleTransparent/TriangleTransparent";
 import { AnimationText } from "../ui/home/AnimationText";
+import { useState } from "react";
 
 export const Home = () => {
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <div className="flex pt-16">
+    <div
+      className={`flex pt-16 ${Style.snip1573} ${isHover ? Style.hover : ""}`}
+    >
       <SplitScreen className="min-h-[calc(100vh-64px)] bg-black" />
       <SplitScreen
         className="min-h-[calc(100vh-64px)] opacity-15"
         bgImage={bgHome}
       />
-      <TriangleTransparent />
+      <TriangleTransparent isHover={isHover} setIsHover={setIsHover} />
       <Triangle />
       <TriangleTwo />
       <AnimationText />
--- frontend/src/components/ui/home/TriangleTransparent/TriangleTransparent.jsx
+++ frontend/src/components/ui/home/TriangleTransparent/TriangleTransparent.jsx
@@ -1,5 +1,26 @@
 import style from "./TriangleTransparent.module.css";
 
-export const TriangleTransparent = () => {
-  return <div className={style.triangleTransparent}></div>;
+export const TriangleTransparent = (prop) => {
+  const { isHover, setIsHover } = prop;
+
+  return (
+    <div
+      className={style.triangleTransparent}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      {isHover && (
+        <div className="w-full h-full flex justify-center items-center text-center bg-transparent z-46">
+          {isHover && (
+            <a
+              href="/#projects"
+              className="bg-black text-4xl text-white p-4 border-2 border-white uppercase tracking-wider z-46 cursor-pointer"
+            >
+              Views my works
+            </a>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
